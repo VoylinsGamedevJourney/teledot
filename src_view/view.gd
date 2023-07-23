6017654195d78f8e1c33ddc42a7ea806eef9f0ff
voylin <0voylin0@gmail.com>
2023-07-23 15:46:59 +0900

    Current progress

--- src_view/view.gd
+++ src_view/view.gd
@@ -1,7 +1,41 @@
 extends ColorRect
 
+const no_connection_text = "[b]No Connection[/b]\nIP: %s\n Port: %s"
+const port := 55757
 
-func change_color_background(new_color: Color) -> void:
+var connected := false
+var show_ip := false
+
+var ip := IP.get_local_addresses() # [0] = public (IPv4), [2] = local
+
+
+func _ready() -> void:
+	# Startup server
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func change_color_background(new_color: Color = Color8(0,0,0)) -> void:
 	self.self_modulate = new_color
-func change_color_text(new_color: Color) -> void:
+func change_color_text(new_color: Color = Color8(255,255,255)) -> void:
 	%Script.self_modulate = new_color
+
+
+func change_script(text: String) -> void:
+	%Script.text = text
+
+
+func _on_switch_ip_button_pressed() -> void:
+	if show_ip:
+		find_child("SwitchIPButton1").text = "Show IP"
+		find_child("SwitchIPButton2").text = "Show IP"
+		%ConnectionInfo.text = no_connection_text % ip[0]
+		show_ip = false
+	else:
+		find_child("SwitchIPButton1").text = "Hide IP"
+		find_child("SwitchIPButton2").text = "Hide IP"
+		%ConnectionInfo.text = no_connection_text % ip[2]
+		show_ip = true
