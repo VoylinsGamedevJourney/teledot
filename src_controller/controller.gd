314fa2683ea703bad1d8294fe2588e424b6ad562
voylin <0voylin0@gmail.com>
2023-07-24 20:24:30 +0900

    Settings menu update

--- src_controller/controller.gd
+++ src_controller/controller.gd
@@ -2,11 +2,9 @@ extends ColorRect
 
 # TODO: Add shortcuts to switch between Script view and preview view
 # TODO: Make scripts saving/deleting/loading work
-# TODO: Save settings and load+set everything on startup
-# TODO: On connection send everything
 
 
-const NET_STATUS := "Status: [i]%s[/i]"
+const SETTINGS_FILE := "user://settings"
 
 const PORT := 55757
 var client: StreamPeerTCP
@@ -16,6 +14,7 @@ var screensaver_img: Texture
 
 
 func _ready() -> void:
+	load_settings()
 	$Screensaver.visible = false
 
 
@@ -25,27 +24,34 @@ func _process(_delta: float) -> void:
 	# Checking status
 	if client.get_status() != status:
 		status = client.get_status()
-		match status:
-			client.STATUS_NONE: 
-				%NetworkStatusLabel.text = NET_STATUS % "[color=grey]no connection[/color]"
-			client.STATUS_ERROR: 
-				%NetworkStatusLabel.text = NET_STATUS % "[color=red]Error[/color]"
-			client.STATUS_CONNECTING: 
-				%NetworkStatusLabel.text = NET_STATUS % "[color=orange]connecting ...[/color]"
-			client.STATUS_CONNECTED: 
-				%NetworkStatusLabel.text = NET_STATUS % "[color=grey]connected[/color]"
-				# TODO: Send all data once
-				send_command("change_script", %ScriptTextEdit.text)
-				send_command("change_alignment", %AlignmentOptionButton.selected) 
-				send_command("change_mirror", %MirrorOptionButton.selected == 1)
-				send_command("change_color_text", %FontColorPicker.color)
-				send_command("change_color_background", %FontColorPicker.color)
-				send_command("change_margin", %MarginSpinBox.value)
-				send_command("change_scroll_speed", %ScrollSpeedSpinBox.value)
-				send_command("change_font_size", %FontSizeSpinBox.value)
+		connection_changed()
+
+
+func connection_changed() -> void:
+	set_connection_text()
+	
+	# Sending all necesarry data to view
+	if status == client.STATUS_CONNECTED: 
+		send_command("change_script", %ScriptTextEdit.text)
+		var settings := FileAccess.open(SETTINGS_FILE, FileAccess.READ)
+		var settings_data: Dictionary = settings.get_var()
+		settings.close()
+		for setting in settings_data:
+			send_command("change_%s" % setting, settings_data[setting])
+
+
+func set_connection_text(_status: int = status) -> void:
+	var text: PackedStringArray
+	match _status:
+		client.STATUS_NONE:       text = ["gray", "no connection"]
+		client.STATUS_ERROR:      text = ["red", "error"]
+		client.STATUS_CONNECTING: text = ["purple", "connecting"]
+		client.STATUS_CONNECTED:  text = ["green", "connected"]
+	%NetworkStatusLabel.text = "Status: [i][color=%s]%s[/color][/i]" % text
 
 
 func send_command(key:String, value) -> void:
+	if client == null: return
 	if client.get_status() == 2: client.put_var([key,value])
 
 
@@ -63,34 +69,80 @@ func _on_connection_button_pressed() -> void:
 	if client == null:
 		# Start connection
 		%ConnectionButton.text = "Stop connection"
+		status = client.STATUS_NONE
 		client = StreamPeerTCP.new()
 		client.connect_to_host(%IPLineEdit.text, int(%PortLineEdit.text.strip_edges()))
-		status = client.STATUS_NONE
 	else:
 		# Stop connection
 		%ConnectionButton.text = "Start connection"
 		client = null
-		%NetworkStatusLabel.text = NET_STATUS % "[color=grey]no connection[/color]"
+		set_connection_text(client.STATUS_NONE)
 
 
 func _on_alignment_option_item_selected(index: int) -> void:
+	save_setting("alignment", index)
 	send_command("change_alignment", index)
-
-
 func _on_mirror_option_button_item_selected(index: int) -> void:
+	save_setting("mirror", index)
 	send_command("change_mirror", index == 1)
-
-
 func _on_font_color_picker_changed(color: Color) -> void:
+	save_setting("color_text", color)
 	send_command("change_color_text", color)
-
-
 func _on_background_color_picker_changed(color: Color) -> void:
+	save_setting("color_background", color)
 	send_command("change_color_background", color)
-
-
 func _on_margin_spin_box_value_changed(value: float) -> void:
+	save_setting("margin", value)
 	send_command("change_margin", value)
+func _on_scroll_speed_spin_box_value_changed(value: float) -> void:
+	save_setting("scroll_speed", value)
+	send_command("change_scroll_speed", value)
+func _on_font_size_spin_box_value_changed(value: float) -> void:
+	save_setting("font_size", value)
+	send_command("change_font_size", value)
+
+
+func save_setting(key: String, value) -> void:
+	var settings_file := FileAccess.open(SETTINGS_FILE, FileAccess.READ)
+	var settings_data: Dictionary = settings_file.get_var()
+	settings_file.close()
+	settings_data[key] = value
+	settings_file = FileAccess.open(SETTINGS_FILE, FileAccess.WRITE)
+	settings_file.store_var(settings_data)
+
+
+func load_settings() -> void:
+	if !FileAccess.file_exists(SETTINGS_FILE):
+		var default_file := FileAccess.open(SETTINGS_FILE,FileAccess.WRITE)
+		default_file.store_var({
+			"color_background": %FontColorPicker.color,
+			"scroll_speed": %ScrollSpeedSpinBox.value,
+			"color_text": %FontColorPicker.color,
+			"alignment": %AlignmentOptionButton.selected,
+			"font_size": %FontSizeSpinBox.value,
+			"mirror": %MirrorOptionButton.selected,
+			"margin": %MarginSpinBox.value,
+			})
+		return
+	var settings_file := FileAccess.open(SETTINGS_FILE, FileAccess.READ)
+	var settings_data: Dictionary = settings_file.get_var()
+	for setting in settings_data:
+		match setting:
+			"alignment": 
+				%AlignmentOptionButton.selected = settings_data[setting]
+			"mirror": 
+				%MirrorOptionButton.selected = settings_data[setting]
+			"color_text": 
+				%FontColorPicker.color = settings_data[setting]
+			"color_background": 
+				%FontColorPicker.color = settings_data[setting]
+			"margin": 
+				%MarginSpinBox.value = settings_data[setting]
+			"scroll_speed": 
+				%ScrollSpeedSpinBox.value = settings_data[setting]
+			"font_size": 
+				%FontSizeSpinBox.value = settings_data[setting]
+			_: printerr("Could not find setting: %s" % setting)
 
 
 # TODO: Screensaver
@@ -99,11 +151,3 @@ func _on_screen_saver_button_pressed() -> void:
 	# TODO: Display screensaver
 	# TODO: when pressed again or esc pressed, exit screensaver mode
 	pass
-
-
-func _on_scroll_speed_spin_box_value_changed(value: float) -> void:
-	send_command("change_scroll_speed", value)
-
-
-func _on_font_size_spin_box_value_changed(value: float) -> void:
-	send_command("change_font_size", value)
